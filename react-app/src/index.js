73b4e69138d7e3111b8cecaefb5477c64ad5dae1
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-11-11 17:17:16 +0100

    Create data dir and add courses to it. Creates components dir with individual courses. Adds all courses to courses.json.

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Workshops from './components/WorkshopsCourseCard'
+import Testing from './components/TestingCourseCard'
+import Vue from './components/VueCourseCard'
+import Productivity from './components/ProductivityCourseCard'
+import NodeReact from './components/NodeReactCourseCard'
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/main" components={Main}></Route>
+      <Route exact path="/workshops" components={Workshops}></Route>
+      <Route exact path="/testing" components={Testing}></Route>
+      <Route exact path="/vue" components={Vue}></Route>
+      <Route exact path="/productivity" components={Productivity}></Route>
+      <Route exact path="/node-react" components={NodeReact}></Route>
+    </Switch>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
